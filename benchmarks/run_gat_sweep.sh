137be0b6ef7b939cd2de0f1be78209a6ec546709
hobogalaxy <zalewski.ukas@gmail.com>
2021-04-09 15:37:44 +0200

    fix incorrect dimensions when having many heads in GAT

--- benchmarks/run_gat_sweep.sh
+++ benchmarks/run_gat_sweep.sh
@@ -5,34 +5,34 @@ logger=wandb logger.wandb.group="GAT_mnist_sp75_optuna" \
 trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
 datamodule.num_workers=12 datamodule.pin_memory=True \
 callbacks.early_stopping.patience=5 \
-+trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
+# +trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
 
-python run.py --config-name config_optuna.yaml --multirun \
-+experiment=GAT/gat_fashion_mnist_sp100 \
-optimized_metric="val/acc_best" \
-logger=wandb logger.wandb.group="GAT_fashion_mnist_sp100_optuna" \
-trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
-datamodule.num_workers=12 datamodule.pin_memory=True \
-callbacks.early_stopping.patience=5 \
-+trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
+# python run.py --config-name config_optuna.yaml --multirun \
+# +experiment=GAT/gat_fashion_mnist_sp100 \
+# optimized_metric="val/acc_best" \
+# logger=wandb logger.wandb.group="GAT_fashion_mnist_sp100_optuna" \
+# trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
+# datamodule.num_workers=12 datamodule.pin_memory=True \
+# callbacks.early_stopping.patience=5 \
+# +trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
 
-python run.py --config-name config_optuna.yaml --multirun \
-+experiment=GAT/gat_cifar10_sp100 \
-optimized_metric="val/acc_best" \
-logger=wandb logger.wandb.group="GAT_cifar10_sp100_optuna" \
-trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
-datamodule.num_workers=12 datamodule.pin_memory=True \
-callbacks.early_stopping.patience=5 \
-+trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
+# python run.py --config-name config_optuna.yaml --multirun \
+# +experiment=GAT/gat_cifar10_sp100 \
+# optimized_metric="val/acc_best" \
+# logger=wandb logger.wandb.group="GAT_cifar10_sp100_optuna" \
+# trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
+# datamodule.num_workers=12 datamodule.pin_memory=True \
+# callbacks.early_stopping.patience=5 \
+# +trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
 
-python run.py --config-name config_optuna.yaml --multirun \
-+experiment=GAT/gat_ogbg_molhiv \
-optimized_metric="val/rocauc_best" \
-logger=wandb logger.wandb.group="GAT_ogbg_molhiv_optuna" \
-trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
-datamodule.num_workers=12 datamodule.pin_memory=True \
-callbacks.early_stopping.patience=5 \
-+trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
+# python run.py --config-name config_optuna.yaml --multirun \
+# +experiment=GAT/gat_ogbg_molhiv \
+# optimized_metric="val/rocauc_best" \
+# logger=wandb logger.wandb.group="GAT_ogbg_molhiv_optuna" \
+# trainer.gpus=1 trainer.min_epochs=10 trainer.max_epochs=100 \
+# datamodule.num_workers=12 datamodule.pin_memory=True \
+# callbacks.early_stopping.patience=5 \
+# +trainer.amp_backend="apex" +trainer.precision=16 +trainer.amp_level="O2"
 
 # python run.py --config-name config_optuna.yaml --multirun \
 # +experiment=GAT/gat_ogbg_molpcba \
