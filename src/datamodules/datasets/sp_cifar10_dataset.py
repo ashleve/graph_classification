fix position normalization in superpixel benchmarks

src/datamodules/datasets/sp_cifar10_dataset.py
@@ -1,11 +1,12 @@
 import os
+from typing import Callable, Optional
 
 import numpy as np
 import torch
-from torch_geometric.data import Data, InMemoryDataset
+from torch_geometric.data import InMemoryDataset
 from torchvision.datasets import CIFAR10
 
-from src.utils.superpixel_generation import convert_torchvision_dataset_to_superpixel_graphs
+from src.utils.superpixel_generation import convert_img_dataset_to_superpixel_graph_dataset
 
 
 class CIFAR10SuperpixelsDataset(InMemoryDataset):
@@ -14,16 +15,19 @@ class CIFAR10SuperpixelsDataset(InMemoryDataset):
     def __init__(
         self,
         root: str = "data/",
-        num_workers: int = 8,
+        num_workers: int = 4,
         n_segments: int = 100,
+        transform: Optional[Callable] = None,
+        pre_transform: Optional[Callable] = None,
+        pre_filter: Optional[Callable] = None,
         **kwargs,
     ):
-        self.data_dir = root
+        self.data_dir = os.path.join(root, "/CIFAR10")
         self.num_workers = num_workers
         self.n_segments = n_segments
         self.slic_kwargs = kwargs
 
-        super().__init__(os.path.join(root, "CIFAR10"))
+        super().__init__(self.data_dir, transform, pre_transform, pre_filter)
 
         self.data, self.slices = torch.load(self.processed_paths[0])
 
@@ -52,12 +56,18 @@ class CIFAR10SuperpixelsDataset(InMemoryDataset):
         labels = np.concatenate((trainset.targets, testset.targets))
         images = np.concatenate((trainset.data, testset.data))
 
-        dataset = (images, labels)
-
-        data, slices = convert_torchvision_dataset_to_superpixel_graphs(
-            dataset=dataset, desired_nodes=self.n_segments, num_workers=self.num_workers
+        data_list = convert_img_dataset_to_superpixel_graph_dataset(
+            images=images,
+            labels=labels,
+            desired_nodes=self.n_segments,
+            num_workers=self.num_workers,
         )
 
-        data = Data(x=data["x"], edge_index=data["edge_index"], pos=data["pos"], y=data["y"])
+        if self.pre_filter is not None:
+            data_list = [data for data in data_list if self.pre_filter(data)]
 
+        if self.pre_transform is not None:
+            data_list = [self.pre_transform(data) for data in data_list]
+
+        data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])

src/datamodules/datasets/sp_fashion_mnist_dataset_8nn.py
@@ -22,6 +22,7 @@ class FashionMNISTSuperpixelsDataset(InMemoryDataset):
         loop: bool = True,
         transform: Optional[Callable] = None,
         pre_transform: Optional[Callable] = None,
+        pre_filter: Optional[Callable] = None,
         **kwargs,
     ):
         self.data_dir = root
@@ -37,7 +38,7 @@ class FashionMNISTSuperpixelsDataset(InMemoryDataset):
                 RadiusGraph(r=r, max_num_neighbors=max_num_neighbors, loop=loop),
             ]
         )
-        super().__init__(os.path.join(root, "FashionMNIST"), transform, pre_transform)
+        super().__init__(os.path.join(root, "FashionMNIST"), transform, pre_transform, pre_filter)
         self.data, self.slices = torch.load(self.processed_paths[0])
 
     @property

src/datamodules/sp_cifar10_datamodule.py
@@ -4,11 +4,12 @@ import torch_geometric.transforms as T
 from pytorch_lightning import LightningDataModule
 from torch.utils.data import random_split
 from torch_geometric.data import DataLoader, Dataset
+from torch_geometric.transforms import NormalizeScale
 
 from src.datamodules.datasets.sp_cifar10_dataset import CIFAR10SuperpixelsDataset
 
 
-class FashionMNISTSuperpixelsDataModule(LightningDataModule):
+class CIFAR10SuperpixelsDataModule(LightningDataModule):
     def __init__(
         self,
         data_dir: str = "data/",
@@ -51,6 +52,14 @@ class FashionMNISTSuperpixelsDataModule(LightningDataModule):
 
         self.slic_kwargs = kwargs
 
+        self.pre_transform = T.Compose(
+            [
+                NormalizeScale(),
+            ]
+        )
+        self.transform = None
+        self.pre_filter = None
+
         self.data_train: Optional[Dataset] = None
         self.data_val: Optional[Dataset] = None
         self.data_test: Optional[Dataset] = None
@@ -70,6 +79,9 @@ class FashionMNISTSuperpixelsDataModule(LightningDataModule):
             self.data_dir,
             n_segments=self.n_segments,
             num_workers=self.sp_generation_workers,
+            transform=self.transform,
+            pre_transform=self.pre_transform,
+            pre_filter=self.pre_filter,
             **self.slic_kwargs,
         )
 
@@ -79,6 +91,9 @@ class FashionMNISTSuperpixelsDataModule(LightningDataModule):
             self.data_dir,
             n_segments=self.n_segments,
             num_workers=self.sp_generation_workers,
+            transform=self.transform,
+            pre_transform=self.pre_transform,
+            pre_filter=self.pre_filter,
             **self.slic_kwargs,
         )
         self.data_train, self.data_val, self.data_test = random_split(

src/utils/superpixel_generation.py
@@ -19,18 +19,8 @@ from tqdm.auto import tqdm
 from src.utils.multiprocessing_istarmap import multiprocessing_istarmap
 
 
-def save_torch_geometric_superpixel_dataset(data_dir, dataset_name: str, data: dict, slices: dict):
-    dataset = Data(x=data["x"], edge_index=data["edge_index"], pos=data["pos"], y=data["y"])
-
-    path = os.path.join(data_dir, dataset_name, "processed")
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    torch.save((dataset, slices), path + "/data.pt")
-
-
-def convert_torchvision_dataset_to_superpixel_graphs(
-    dataset, desired_nodes: int = 75, num_workers: int = 4, **slic_kwargs
+def convert_img_dataset_to_superpixel_graph_dataset(
+    images, labels, desired_nodes: int = 75, num_workers: int = 4, **slic_kwargs
 ):
     """Convert torchvision dataset to superpixel graphs
 
@@ -44,18 +34,9 @@ def convert_torchvision_dataset_to_superpixel_graphs(
             Don't use too much or it will crash.
     """
 
-    images, labels = dataset
+    assert len(images) == len(labels)
 
-    # first we gather all graphs into lists, then we convert them into torch matrixes
-    x_list = []
-    edge_index_list = []
-    pos_list = []
-
-    # slices to know when given graphs starts and stops in a dataset matrix
-    x_slices_list = [0]
-    edge_index_slices_list = [0]
-    pos_slices_list = [0]
-    y_slices_list = [0]
+    data_list = []
 
     start_time = time.time()
 
@@ -63,7 +44,7 @@ def convert_torchvision_dataset_to_superpixel_graphs(
     multiprocessing.pool.Pool.istarmap = multiprocessing_istarmap
 
     with multiprocessing.Pool(num_workers) as pool:
-        args = list(zip(images, repeat(desired_nodes), repeat(slic_kwargs)))
+        args = list(zip(images, labels, repeat(desired_nodes), repeat(slic_kwargs)))
 
         for graph in tqdm(
             pool.istarmap(convert_numpy_img_to_superpixel_graph, args),
@@ -71,40 +52,20 @@ def convert_torchvision_dataset_to_superpixel_graphs(
             desc="Generating superpixels",
             colour="GREEN",
         ):
-            x, edge_index, pos = graph
-
-            x_list.append(torch.as_tensor(x, dtype=torch.float32))
-            edge_index_list.append(torch.as_tensor(edge_index, dtype=torch.long))
-            pos_list.append(torch.as_tensor(pos, dtype=torch.float32))
-
-            x_slices_list.append(x_slices_list[-1] + len(x))
-            edge_index_slices_list.append(edge_index_slices_list[-1] + len(edge_index))
-            pos_slices_list.append(pos_slices_list[-1] + len(pos))
-            y_slices_list.append(y_slices_list[-1] + 1)
+            x, edge_index, pos, y = graph
+            x = torch.as_tensor(x, dtype=torch.float32)
+            edge_index = torch.as_tensor(edge_index, dtype=torch.long).T
+            pos = torch.as_tensor(pos, dtype=torch.float32)
+            y = torch.as_tensor(y, dtype=torch.long)
+            data_list.append(Data(x=x, edge_index=edge_index, pos=pos, y=y))
 
     total_time = time.time() - start_time
     print(f"Took {total_time}s.")
 
-    data = {}
-    data["x"] = torch.cat(x_list, dim=0)
-    data["edge_index"] = torch.cat(edge_index_list, dim=0).T
-    data["pos"] = torch.cat(pos_list, dim=0)
-    data["y"] = torch.as_tensor(labels, dtype=torch.long)
-
-    del x_list, edge_index_list, pos_list
-
-    slices = {}
-    slices["x"] = torch.as_tensor(x_slices_list, dtype=torch.long)
-    slices["edge_index"] = torch.as_tensor(edge_index_slices_list, dtype=torch.long)
-    slices["pos"] = torch.as_tensor(pos_slices_list, dtype=torch.long)
-    slices["y"] = torch.as_tensor(y_slices_list, dtype=torch.long)
-
-    del x_slices_list, edge_index_slices_list, pos_slices_list, y_slices_list
-
-    return data, slices
+    return data_list
 
 
-def convert_numpy_img_to_superpixel_graph(img, desired_nodes: int = 75, slic_kwargs={}):
+def convert_numpy_img_to_superpixel_graph(img, label, desired_nodes: int = 75, slic_kwargs={}):
     """Convert numpy img to superpixel grap.
     Each superpixel is connected to all superpixels it directly touches.
 
@@ -179,39 +140,6 @@ def convert_numpy_img_to_superpixel_graph(img, desired_nodes: int = 75, slic_kwa
     for node in G.nodes:
         x[node, :] = G.nodes[node]["features"]
 
-    return x, edge_index, pos
+    y = label
 
-
-if __name__ == "__main__":
-    from torchvision.datasets import CIFAR10, MNIST, FashionMNIST
-
-    trainset = FashionMNIST("data/", download=True, train=True)
-    testset = FashionMNIST("data/", download=True, train=False)
-
-    train_images = trainset.data
-    test_images = testset.data
-    print(train_images.shape)
-    print(test_images.shape)
-
-    images = np.concatenate((train_images, test_images))
-
-    # don't do reshape for cifar10!
-    images = np.reshape(images, (len(images), 28, 28, 1))
-    print(images.shape)
-
-    train_labels = trainset.targets
-    test_labels = testset.targets
-    labels = np.concatenate((train_labels, test_labels))
-    print(labels.shape)
-
-    dataset = (images, labels)
-    data, slices = convert_torchvision_dataset_to_superpixel_graphs(
-        dataset, desired_nodes=100, num_workers=10
-    )
-
-    # torch_dataset = Data(x=data["x"], edge_index=data["edge_index"], pos=data["pos"], y=data["y"])
-    # print(torch_dataset)
-
-    save_torch_geometric_superpixel_dataset(
-        data_dir="data/", dataset_name="fahion_mnist_sp100", data=data, slices=slices
-    )
+    return x, edge_index, pos, y